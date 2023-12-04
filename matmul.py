96960245699f54381d657bd1656cd64a8c4ba4ac
Carter Fulcher <carter@fulchereng.com>
2023-12-03 22:51:02 -0700

    naive matmul

--- matmul.py
+++ matmul.py
@@ -1,13 +1,36 @@
 import numpy as np 
 import time 
 
-x = np.random.rand(1024, 1024)
-y = np.random.rand(1024, 1024)
+N = 8
 
-st = time.monotonic()
-z = np.dot(x, y)
-print("np.dot: ", time.monotonic() - st)
+x = np.random.rand(N, N)
+y = np.random.rand(N, N)
 
 st = time.monotonic()
 z = x @ y
-print("@: ", time.monotonic() - st)
+
+at_time = time.monotonic() - st
+
+print("@: ", at_time)
+
+""" Naive matmul """
+def matmul(A: np.ndarray, B: np.ndarray) -> np.ndarray:
+  # A is size m x n, B is size n x p. C (output) is size m x p. Multiply->Add
+  assert A.shape[1] == B.shape[0]
+  C, n = np.zeros((A.shape[0], B.shape[1])), A.shape[1] 
+  for i in range(A.shape[0]):
+    for j in range(B.shape[0]):
+      C[i][j] = sum([A[i][k] * B[k][j] for k in range(n)])
+
+  return C
+
+st = time.monotonic()
+C = matmul(x, y)
+matmul_time = time.monotonic() - st
+print(f"matmul: {matmul_time}")
+
+exp = C.all() == z.all()
+print(f"CHECK: {exp}")
+assert exp
+
+print(f"@ is {matmul_time/at_time:.2f} faster than matmul")
